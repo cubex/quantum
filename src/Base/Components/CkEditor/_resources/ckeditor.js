db8e3fe2bf9fb41299c06eb4716de5ea1e1e0b90
Tom Kay <oridan82@gmail.com>
2019-04-03 10:38:58 +0100

    delay creation of ckeditor

--- src/Base/Components/CkEditor/_resources/ckeditor.js
+++ src/Base/Components/CkEditor/_resources/ckeditor.js
@@ -1,34 +1,41 @@
 window.QuantumEditorConfig = window.QuantumEditorConfig || {};
 
-QuantumWidgetEditor
-  .create(
-    document.querySelector('.content-editor'),
-    Object.assign(
-      { // enforced
-        iframe: true,
-      },
-      QuantumEditorConfig,
-      { // defaults
-        filer: {
-          // Upload the images to the server using the CKFinder QuickUpload command.
-          uploadUrl: '/uploads/filer',
+// delay creating editor
+setTimeout(
+  function ()
+  {
+    QuantumWidgetEditor
+      .create(
+        document.querySelector('.content-editor'),
+        Object.assign(
+          { // enforced
+            iframe: true,
+          },
+          QuantumEditorConfig,
+          { // defaults
+            filer: {
+              // Upload the images to the server using the CKFinder QuickUpload command.
+              uploadUrl: '/uploads/filer',
 
-          // Define the CKFinder configuration (if necessary).
-          options: {
-            meta: ['image', 'video']
+              // Define the CKFinder configuration (if necessary).
+              options: {
+                meta: ['image', 'video']
+              }
+            }
           }
+        )
+      )
+      .then(
+        function (editor)
+        {
+
+          //    import {toWidget} from '@ckeditor/ckeditor5-widget/src/utils';
+          //    import {downcastElementToElement} from '@ckeditor/ckeditor5-engine/src/conversion/downcast-converters';
+          //    import {upcastElementToElement} from '@ckeditor/ckeditor5-engine/src/conversion/upcast-converters';
+
         }
-      }
-    )
-  )
-  .then(
-    editor =>
-    {
-
-      //    import {toWidget} from '@ckeditor/ckeditor5-widget/src/utils';
-      //    import {downcastElementToElement} from '@ckeditor/ckeditor5-engine/src/conversion/downcast-converters';
-      //    import {upcastElementToElement} from '@ckeditor/ckeditor5-engine/src/conversion/upcast-converters';
-
-    }
-  )
-  .catch(error => {console.error(error);});
+      )
+      .catch(function (error) {console.error(error);});
+  },
+  0
+);
