2e36ed91453e320997c3ec75aa1e588896e57b83
Tom Kay <oridan82@gmail.com>
2019-04-09 13:42:16 +0100

    include ck styles inside ContentController

--- src/Modules/Pages/Controllers/ContentController.php
+++ src/Modules/Pages/Controllers/ContentController.php
@@ -1,10 +1,12 @@
 <?php
 namespace Cubex\Quantum\Modules\Pages\Controllers;
 
+use Cubex\Quantum\Base\Components\CkEditor\CkEditorComponent;
 use Cubex\Quantum\Base\Controllers\QuantumBaseController;
 use Cubex\Quantum\Base\Interfaces\QuantumFrontendHandler;
 use Cubex\Quantum\Modules\Pages\Daos\Page;
 use Cubex\Quantum\Modules\Pages\Daos\PageContent;
+use Packaged\Dispatch\ResourceManager;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
 class ContentController extends QuantumBaseController implements QuantumFrontendHandler
@@ -26,6 +28,8 @@ class ContentController extends QuantumBaseController implements QuantumFrontend
 
   public function processDefault()
   {
+    ResourceManager::component(new CkEditorComponent())->requireCss('include.css');
+
     $page = Page::loadById($this->_options->get('pageId'));
     $content = PageContent::loadById($page->id, $page->publishedVersion);
 
