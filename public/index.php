243495b4c0869ed8d8caea62e2dbb65bdabcba21
Tom Kay <oridan82@gmail.com>
2019-05-20 10:36:34 +0100

    clean up index

--- public/index.php
+++ public/index.php
@@ -1,28 +1,20 @@
 <?php
 define('PHP_START', microtime(true));
 
-use Cubex\Context\Context;
 use Cubex\Cubex;
 use Cubex\Quantum\Example\Project;
 use Whoops\Handler\PrettyPageHandler;
 use Whoops\Run;
 
-$isDev = true;
 $loader = require_once(dirname(__DIR__) . '/vendor/autoload.php');
 $cubex = new Cubex(dirname(__DIR__), $loader);
-//$launcher->listen(Cubex::EVENT_HANDLE_START, function (Context $ctx) { /* Configure your request here  */ });
 try
 {
   $project = new Project($cubex);
-  $cubex->handle($project, true, !$cubex->getContext()->isEnv(Context::ENV_LOCAL));
+  $cubex->handle($project, true);
 }
 catch(Throwable $e)
 {
-  if(!$isDev)
-  {
-    die('Unable to handle your request');
-  }
-
   $handler = new Run();
   $handler->pushHandler(new PrettyPageHandler());
   $handler->handleException($e);
